3c77cb16f7c4a0f6cce92c598bf87ee704b60c48
thoffmann <toni.hoffmann@arthrex.de>
2018-03-08 10:50:32 +0100

    init empty swift project

---
+++ SwiftDebouncer/SwiftDebouncer.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDebouncer.h
+//  SwiftDebouncer
+//
+//  Created by Toni Hoffmann on 08.03.18.
+//  Copyright © 2018 Toni Hoffmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDebouncer.
+FOUNDATION_EXPORT double SwiftDebouncerVersionNumber;
+
+//! Project version string for SwiftDebouncer.
+FOUNDATION_EXPORT const unsigned char SwiftDebouncerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDebouncer/PublicHeader.h>
+
+
