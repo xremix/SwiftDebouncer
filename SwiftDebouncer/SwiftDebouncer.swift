272d45219ec772c4501d8d1ddb2b42de22010f48
thoffmann <toni.hoffmann@arthrex.de>
2018-03-08 10:53:55 +0100

    adding empty class

---
+++ SwiftDebouncer/SwiftDebouncer.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftDebouncer.swift
+//  SwiftDebouncer
+//
+//  Created by Toni Hoffmann on 08.03.18.
+//  Copyright © 2018 Toni Hoffmann. All rights reserved.
+//
+
+import UIKit
+
+class SwiftDebouncer: NSObject {
+
+}
