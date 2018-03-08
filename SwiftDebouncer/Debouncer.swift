fde9b167192a308022fcf82ffe9a62809c134716
thoffmann <toni.hoffmann@arthrex.de>
2018-03-08 11:50:00 +0100

    impelement debounce class and tests

--- SwiftDebouncer/Debouncer.swift
+++ SwiftDebouncer/Debouncer.swift
@@ -1,6 +1,6 @@
 //
-//  SwiftDebouncer.swift
-//  SwiftDebouncer
+//  Debouncer.swift
+//  Debouncer
 //
 //  Created by Toni Hoffmann on 08.03.18.
 //  Copyright © 2018 Toni Hoffmann. All rights reserved.
@@ -9,5 +9,29 @@
 import UIKit
 
 public class Debouncer: NSObject {
-
+    var callback: (() -> Void)?
+    let delay: TimeInterval
+    private var timer: Timer?
+    
+    init(delay: TimeInterval){
+        self.delay = delay
+    }
+    
+    init(delay: TimeInterval, callback: @escaping (()->Void)){
+        self.delay = delay
+        self.callback = callback
+    }
+    
+    func call(){
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: delay, target: self, selector: #selector(timerCalled), userInfo: nil, repeats: false)
+    }
+    @objc private func timerCalled(_ timer: Timer) {
+        if callback == nil {
+            NSLog("Debouncer timer fired, but callback was nil")
+        } else {
+            NSLog("Debouncer timer fired")
+        }
+        callback?()
+    }
 }
